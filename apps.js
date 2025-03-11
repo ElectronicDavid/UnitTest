d272e6b41ae2cf2376443bfe09c86214933befc0
David Vargas <davidavq@gmail.com>
2025-03-10 21:35:12 -0500

    More Unit Test

--- apps.js
+++ apps.js
@@ -6,5 +6,22 @@ function Suma (a,b) {
     return a + b;
 } 
 
-export {Hello, Suma};
+function canDrinkInCol(year){
+    if(year < 18){
+        return false;
+    }
+    else return true;
+} 
+
+function orderArry(arr) {
+    return arr.sort((a, b) => a - b); 
+}
+
+function isPalindrome(text) {
+    let clean = text.toLowerCase().replace(/[^a-z0-9]/g, ""); // Convertir a minúsculas y eliminar caracteres especiales
+    let invested = clean.split("").reverse().join(""); // Invertir la cadena
+    return clean === invested;
+}
+
+export {Hello, Suma, canDrinkInCol, orderArry,isPalindrome};
 
--- test/test.js
+++ test/test.js
@@ -1,26 +1,47 @@
 import {assert ,expect, should} from 'chai'; 
-import {Hello, Suma, isPalindrome}  from '../apps.js'; 
+import {Hello, Suma, canDrinkInCol, orderArry,isPalindrome}  from '../apps.js'; 
 should(); 
 
-describe('App', function(){
-    it('Assert', function(){
+describe('Units Testing with Chai', function(){
+    it('Returning Hello with Assert', function(){
         assert.equal(Hello(),'hello');
     })
 
-    it('Expect', function(){
+    it('Returning Hello with expect', function(){
         expect(Hello()).to.equal('hello');
     })
 
-    it('Should', function(){
+    it('Returning Hello with Should', function(){
         Hello().should.equal('hello');
     })
 
-    it('Should be 8', function(){
+    it('Sum should be 8a with assert', function(){
         assert.equal(Suma(4,4),8);
     })
 
-    it('Should ', function(){
+    it('Sum should be 8a with should', function(){
         assert.equal(Suma(4,4),8);
+    })
+
+    it('Should return True if the age is under 18', function(){
+        expect(canDrinkInCol(17)).to.equal(false);
+    })
+
+    it('Should return True if the age is above 18', function(){
+        expect(canDrinkInCol(100)).to.equal(true);
+    })
+
+    it('Should return the ordered array ', function(){
+        let numbers = [5, 3, 9, 1, 4];
+        orderArry(numbers).should.not.equal([ 1, 3, 4, 5, 9 ]);
+    })
+
+    it('Anita lava la tina should return true', function(){
+        assert.equal(isPalindrome("Anita lava la tina"),true);
+    })
+
+    it('Hello World should return false', function(){
+        assert.equal(isPalindrome("Hello World"),false);
     })
 
 })
